69f7cb07e251c9e5ea64b7f3d0e4a0601e7f87e6
Hallvard Jore Christensen <hallvard.jore.christensen@gmail.com>
2016-01-20 15:31:06 +0100

    Main file to start the ecj evolution

---
+++ src/com/hajoch/Main.java
@@ -0,0 +1,11 @@
+package com.hajoch;
+
+import ec.Evolve;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String [] params = new String[]{"-file", "EvolutionRunner\\src\\com\\hajoch\\problem.params"};
+        Evolve.main(params);
+    }
+}
