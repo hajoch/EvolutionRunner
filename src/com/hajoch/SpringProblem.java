d9f0b0a6e11569d481ee73ba546488a334ae3f1c
Jonatan Hoff <jona341@hotmail.com>
2016-01-25 15:04:50 +0100

    added print statement

--- src/com/hajoch/SpringProblem.java
+++ src/com/hajoch/SpringProblem.java
@@ -21,28 +21,27 @@ public class SpringProblem extends GPProblem implements SimpleProblemForm {
     // TODO  maybe replace start_time with generation and individual, or of the like
     private final long START_TIME = System.currentTimeMillis();
     // Path to the script that runs Zero-K
-    private final String SCRIPT_URL = "\\script\\runHeadless.bat";
+    private final String SCRIPT_URL = "src\\script\\runHeadless.bat";
     // Path to where the BehaviourTree individual and gameLog should be saved
     private final String OUT_URL = "out\\";
 
     /**
-     *
-     * @param evolutionState    evolutionstate.
-     * @param individual        individual
-     * @param i                 i
-     * @param i1                i1
+     * @param evolutionState evolutionstate.
+     * @param individual     individual
+     * @param i              i
+     * @param i1             i1
      */
     @Override
     public void evaluate(EvolutionState evolutionState, Individual individual, int i, int i1) {
-        if(!individual.evaluated) {
-            BooleanData input = (BooleanData)this.input;
-            String treeString = (((GPIndividual)individual).trees[0].child).toString();
+        if (!individual.evaluated) {
+            BooleanData input = (BooleanData) this.input;
+            String treeString = (((GPIndividual) individual).trees[0].child).toString();
             // Write BT individual to text file for the Spring Bot to read and parse
             writeToFile("bt", Collections.singletonList(treeString));
 
             double f = getFitness();
 
-            KozaFitness fitness = (KozaFitness)individual.fitness;
+            KozaFitness fitness = (KozaFitness) individual.fitness;
             fitness.setStandardizedFitness(evolutionState, f);
             individual.evaluated = true;
         }
@@ -51,7 +50,8 @@ public class SpringProblem extends GPProblem implements SimpleProblemForm {
     /**
      * Run game, interpret results and return fitness.
      * NB: This method is problem specific.
-     * @return                  fitness (Kozafirness)
+     *
+     * @return fitness (Kozafirness)
      */
     private double getFitness() {
 
@@ -70,13 +70,15 @@ public class SpringProblem extends GPProblem implements SimpleProblemForm {
 
             // Read console output
             String line;
-            while(process.isAlive()) {
+            while (process.isAlive()) {
                 line = reader.readLine();
-                if(null != line) {
+                if (null != line) {
                     output.add(line);
                 }
             }
-        } catch (IOException e) { e.printStackTrace(); }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
         // Write the console output to file
@@ -87,47 +89,51 @@ public class SpringProblem extends GPProblem implements SimpleProblemForm {
         int time = 0;
         int avgEco = 0;
         int soldiers = 0;
-        for(String s: output){
-            if(s.contains("game_message: Alliance ")){
-                winner = Integer.parseInt(s.substring(s.indexOf("Alliance ")+9, s.indexOf(" wins")));
+        for (String s : output) {
+            if (s.contains("game_message: Alliance ")) {
+                winner = Integer.parseInt(s.substring(s.indexOf("Alliance ") + 9, s.indexOf(" wins")));
             }
-            if(s.contains("END")){
-                teamid = Integer.parseInt(s.substring(s.indexOf("teamid: ")+8, s.indexOf(" time: ")));
-                time = Integer.parseInt(s.substring(s.indexOf("time: ")+6, s.indexOf(" Soldiers: ")));
-                soldiers = Integer.parseInt(s.substring(s.indexOf("Soldiers: ")+10, s.indexOf(" avgEco: ")));
-                avgEco = Integer.parseInt(s.substring(s.indexOf("avgEco: ")+8, s.length()));
+            if (s.contains("END")) {
+                teamid = Integer.parseInt(s.substring(s.indexOf("teamid: ") + 8, s.indexOf(" time: ")));
+                time = Integer.parseInt(s.substring(s.indexOf("time: ") + 6, s.indexOf(" Soldiers: ")));
+                soldiers = Integer.parseInt(s.substring(s.indexOf("Soldiers: ") + 10, s.indexOf(" avgEco: ")));
+                avgEco = Integer.parseInt(s.substring(s.indexOf("avgEco: ") + 8, s.length()));
             }
         }
 
-        if(winner == teamid)
+        if (winner == teamid) {
+            System.out.println("We won" + " Time: " + time + " Soldiers: " + soldiers + " avgEco: "+ avgEco);
             return 1;
-        else
+        } else {
+            System.out.println("We lost" + " Time: " + time + " Soldiers: " + soldiers + " avgEco: "+ avgEco);
             return -1;
+        }
         // Calculate the fitness //TODO
     }
 
     /**
      * Creates a new file in the OUT_URL containing the content and the given name.
-     * @param name              filename. Will be postfixed with the time at run-start.
-     * @param content           content of the file
-     * @return                  returns true if no error occurred during the process
+     *
+     * @param name    filename. Will be postfixed with the time at run-start.
+     * @param content content of the file
+     * @return returns true if no error occurred during the process
      */
-    private boolean writeToFile(String name, List<String> content){
+    private boolean writeToFile(String name, List<String> content) {
 
         PrintWriter printer;
 
         File file = new File(new StringBuilder(OUT_URL).append(name).append(START_TIME).append(".txt").toString());
         try {
             file.getParentFile().mkdirs();
-            if(!file.exists())
+            if (!file.exists())
                 file.createNewFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
         try {
             printer = new PrintWriter(file);
-            for(String s : content)
-                printer.write(s+"\n");
+            for (String s : content)
+                printer.write(s + "\n");
             printer.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
